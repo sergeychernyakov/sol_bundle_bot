812589a313d842e95c791b38bfd27a097bfab207
Avagyan02 <samvel.avagyan.02@bk.ru>
2024-12-05 16:08:44 +0400

    changes about token buying functionality

--- src/services/buy_tokens_service.ts
+++ src/services/buy_tokens_service.ts
@@ -21,6 +21,23 @@ export class BuyTokensService {
     const totalAmountInput: string = readlineSync.question('Введите общее количество токенов для покупки (по умолчанию 20000): ');
     const amount: number = parseFloat(totalAmountInput) || 20000;
 
+    const amounts: number[] = this.distributeAmount(amount, wallets.length);
+    console.log('Amounts distributed among wallets:', amounts);
+
+    // Вывод списка транзакций и запрос подтверждения
+    console.log('\nСписок транзакций для отправки:');
+    for (let i = 0; i < wallets.length; i++) {
+      console.log(`Транзакция ${i + 1}:`);
+      console.log(`Кошелёк: ${wallets[i].publicKey.toString()}`);
+      console.log(`Сумма покупки (CHEESE): ${amounts[i]}`);
+    }
+
+    const confirmation = readlineSync.question('\nОтправить транзакции? (y/n): ');
+    if (!['да', 'д', 'y', 'yes'].includes(confirmation.toLowerCase())) {
+      console.log('Отправка транзакций отменена пользователем.');
+      return;
+    }
+
     for (const wallet of wallets) {
       const transaction = new Transaction().add(
         new TransactionInstruction({
@@ -33,5 +50,38 @@ export class BuyTokensService {
       await bundleTransaction([transaction], [wallet]);
       console.log(`Purchased tokens with wallet: ${wallet.publicKey.toString()}`);
     }
+  }
+
+  private distributeAmount(totalAmount: number, numberOfWallets: number): number[] {
+    console.log(`Distributing total amount ${totalAmount} among ${numberOfWallets} wallets.`);
+    let randomNumbers: number[] = [];
+    for (let i = 0; i < numberOfWallets; i++) {
+      randomNumbers.push(Math.random());
+    }
+    console.log('Random numbers generated:', randomNumbers);
+
+    const sum: number = randomNumbers.reduce((a, b) => a + b, 0);
+    randomNumbers = randomNumbers.map((num) => num / sum);
+
+    let amounts: number[] = randomNumbers.map((num) => num * totalAmount);
+
+    amounts = amounts.map((num) => parseFloat(num.toFixed(6)));
+
+    const adjustedAmounts: number[] = this.adjustAmounts(amounts, totalAmount);
+    console.log('Adjusted amounts:', adjustedAmounts);
+
+    return adjustedAmounts;
+  }
+
+  private adjustAmounts(amounts: number[], totalAmount: number): number[] {
+    console.log('Adjusting amounts to match total amount.');
+    const sum: number = amounts.reduce((a, b) => a + b, 0);
+    const diff: number = totalAmount - sum;
+
+    console.log(`Sum of amounts: ${sum}, Difference: ${diff}`);
+
+    amounts[0] += diff;
+
+    return amounts;
   }
 }
